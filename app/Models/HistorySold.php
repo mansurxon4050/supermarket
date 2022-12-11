f4cf9cbe819da086a3398424041bfd94b11dcac4
mansurxon4050 <mansurxonabdurashidovhp@gmail.com>
2022-12-11 21:28:18 +0500

    fillable

--- app/Models/HistorySold.php
+++ app/Models/HistorySold.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class HistorySold extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'payment_type',
+        'total_price',
+        'address',
+        'muljal',
+        'address_phone_number',
+        'long',
+        'lat',
+        'data'
+    ];
 }
