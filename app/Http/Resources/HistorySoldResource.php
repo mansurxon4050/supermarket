7c504d6c0869b1d939b41e34915611dbcfe017b2
mansurxon4050 <mansurxonabdurashidovhp@gmail.com>
2022-12-14 09:46:08 +0500

    ss HistoryResource extends JsonResource

---
+++ app/Http/Resources/HistorySoldResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HistorySoldResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'user_id' => $this->user_id,
+            'payment_type' => $this->payment_type,
+            'total_price' => $this->total_price,
+            'address' => $this->address,
+            'muljal' => $this->muljal,
+            'address_phone_number' => $this->address_phone_number,
+            'long' => $this->long,
+            'lat' => $this->lat,
+            'data' => $this->data,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
