84561d847badea89ba1ce6810ef9a5cf4fbbd2ce
mansurxon4050 <mansurxonabdurashidovhp@gmail.com>
2022-12-14 09:27:22 +0500

    this->id

--- app/Http/Controllers/HistorySoldController.php
+++ app/Http/Controllers/HistorySoldController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\HistoryResource;
 use App\Models\HistorySold;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class HistorySoldController extends Controller
