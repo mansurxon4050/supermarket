604b7018343275af7d7b930b0cb9b52848389dd8
mansurxon4050 <mansurxonabdurashidovhp@gmail.com>
2022-12-14 17:00:00 +0500

    $historyData = json_decode($history->data, true, 512, JSON_THROW_ON_ERROR);

--- app/Http/Controllers/HistorySoldController.php
+++ app/Http/Controllers/HistorySoldController.php
@@ -103,7 +103,7 @@ class HistorySoldController extends Controller
     {
         $history=HistorySold::find($request->id);
         $newArray=[];
-        $historyData = json_decode($history->data, true, 512, JSON_THROW_ON_ERROR);
-        return $historyData;
+
+        return $history->data;
     }
 }
