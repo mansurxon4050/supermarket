ffefd31461ab1cdba3ec43d42501fbe8d172481f
mansurxon4050 <mansurxonabdurashidovhp@gmail.com>
2022-12-23 13:32:18 +0500

    auth all

--- routes/api.php
+++ routes/api.php
@@ -22,37 +22,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('history/sold',[HistorySoldController::class,'create']);
-
-/// category
-Route::get('category/products',[ProductController::class,'index']);
-Route::get('category',[CategoryController::class,'index']);
-
-/// home
-Route::get('product/popular',[HomeController::class,'popular']);
-Route::get('product/images',[HomeController::class,'images']);
-Route::get('product/item',[ProductController::class,'item']);
-
-/// star add
-Route::post('product/star/add',[ProductController::class,'star_add']);
-
-/// favorite
-Route::post('product/favorite/add',[UserController::class,'favorite_add']);
-Route::get('product/favorite/index',[UserController::class,'favorite_index']);
-Route::delete('product/favorite/delete',[UserController::class,'favorite_delete']);
-
-/// history
-Route::get('history/index',[HistorySoldController::class,'history_index']);
-Route::get('history/all',[HistorySoldController::class,'historyAll']);
-Route::get('history/accepted',[HistorySoldController::class,'history_accepted']);
-Route::delete('history/delete',[HistorySoldController::class,'history_delete']);
-///search
-Route::get('search',[ProductController::class,'search']);
-Route::get('discount',[ProductController::class,'discount']);
-
-// person
-Route::get('news',[HomeController::class,'news']);
-
 /// register
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
@@ -60,33 +29,59 @@ Route::post('update_check',[AuthController::class,'update_check']);
 Route::post('upload',[ImageController::class,'upload']);
 
 Route::middleware('auth:sanctum')->group(function (){
+/// register auth
     Route::get( 'me',[AuthController::class,'user']);
     Route::post('logout',[AuthController::class,'logout']);
     Route::post('update_password',[AuthController::class,'update_password']);
-    Route::get( 'admin/users/index',[UserController::class,'index']);
-    Route::post('admin/users/update',[UserController::class,'update_role']);
-    Route::get( 'admin/product/index',[ProductController::class,'index_all']);
-    Route::post('admin/product/create',[ProductController::class,'create_product']);
-    Route::post('admin/product/update',[ProductController::class,'update_product']);
-    Route::post('admin/product/update_str',[ProductController::class,'update_productNoImage']);
-    Route::delete('admin/product/delete',[ProductController::class,'delete_product']);
-    Route::post('admin/category/add',[CategoryController::class,'category_add']);
-    Route::post('admin/category/update',[CategoryController::class,'category_update']);
-    Route::post('admin/category/update_str',[CategoryController::class,'category_updateNoImage']);
-    Route::delete('admin/category/delete',[CategoryController::class,'delete_category']);
+///Banner
     Route::get('banner/index',[CategoryController::class,'banner_index']);
-    Route::post('admin/banner/add',[CategoryController::class,'banner_add']);
-    Route::post('admin/banner/update',[CategoryController::class,'banner_update']);
-    Route::delete('admin/banner/delete',[CategoryController::class,'banner_delete']);
+///News
+    Route::get('news',[HomeController::class,'news']);
 
+/// category
+    Route::get('category/products',[ProductController::class,'index']);
+    Route::get('category',[CategoryController::class,'index']);
 
-    // Route::apiResource('waters',WaterController::class);
-    /*
-    Route::get('users',[UserController::class,'index']);
-    Route::post('users',[UserController::class,'store']);
-    Route::get('users/{id}',[UserController::class,'show']);
-    Route::put('users/{id}',[UserController::class,'update']);
-    Route::delete('users/{id}',[UserController::class,'destroy']);*/
+/// home
+    Route::get('product/popular',[HomeController::class,'popular']);
+    Route::get('product/images',[HomeController::class,'images']);
+    Route::get('product/item',[ProductController::class,'item']);
 
+/// star add
+    Route::post('product/star/add',[ProductController::class,'star_add']);
+
+/// favorite
+    Route::post('product/favorite/add',[UserController::class,'favorite_add']);
+    Route::get('product/favorite/index',[UserController::class,'favorite_index']);
+    Route::delete('product/favorite/delete',[UserController::class,'favorite_delete']);
+
+/// history
+    Route::get('history/index',[HistorySoldController::class,'history_index']);
+    Route::get('history/all',[HistorySoldController::class,'historyAll']);
+    Route::get('history/accepted',[HistorySoldController::class,'history_accepted']);
+    Route::delete('history/delete',[HistorySoldController::class,'history_delete']);
+    Route::post('history/sold',[HistorySoldController::class,'create']);
+
+///search
+    Route::get('search',[ProductController::class,'search']);
+    Route::get('discount',[ProductController::class,'discount']);
+
+    /// admin
+    Route::prefix('/admin')->group(function () {
+    Route::get( '/users/index',[UserController::class,'index']);
+    Route::post('/users/update',[UserController::class,'update_role']);
+    Route::get( '/product/index',[ProductController::class,'index_all']);
+    Route::post('/product/create',[ProductController::class,'create_product']);
+    Route::post('/product/update',[ProductController::class,'update_product']);
+    Route::post('/product/update_str',[ProductController::class,'update_productNoImage']);
+    Route::delete('/product/delete',[ProductController::class,'delete_product']);
+    Route::post('/category/add',[CategoryController::class,'category_add']);
+    Route::post('/category/update',[CategoryController::class,'category_update']);
+    Route::post('/category/update_str',[CategoryController::class,'category_updateNoImage']);
+    Route::delete('/category/delete',[CategoryController::class,'delete_category']);
+    Route::post('/banner/add',[CategoryController::class,'banner_add']);
+    Route::post('/banner/update',[CategoryController::class,'banner_update']);
+    Route::delete('/banner/delete',[CategoryController::class,'banner_delete']);
+    });
 
 });
